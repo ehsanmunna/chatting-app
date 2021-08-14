015bc0cdcf868034dfd4cacc0ba2b62ff5ae533d
ehsan munna <grmunnabd@gmail.com>
2021-08-14 12:06:42 +0600

    basic flow complete

--- index.js
+++ index.js
@@ -10,13 +10,20 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket)=>{
+  // socket.broadcast.emit('hi');
+
   console.log('a user connected');
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+    // console.log('message: ' + msg);
+    socket.broadcast.emit('chat message', msg);
+    socket.emit('chat message', msg);
   });
+
 })
 
 server.listen(3000, () => {
